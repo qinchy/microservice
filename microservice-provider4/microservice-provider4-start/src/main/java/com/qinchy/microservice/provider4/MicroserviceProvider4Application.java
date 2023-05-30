3503dfac80110d8a6c34c481333b83e705b6b61c
qinchy <qinchy@msn.com>
2023-05-30 10:24:17 +0800

    整合skywalking

--- microservice-provider4/microservice-provider4-start/src/main/java/com/qinchy/microservice/provider4/MicroserviceProvider4Application.java
+++ microservice-provider4/microservice-provider4-start/src/main/java/com/qinchy/microservice/provider4/MicroserviceProvider4Application.java
@@ -1,4 +1,4 @@
-package com.qinchy.microserviceprovider4;
+package com.qinchy.microservice.provider4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
